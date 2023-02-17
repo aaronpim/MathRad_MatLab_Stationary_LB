904dab43dfcbc11731a3661d2ba1db5e0b4c9ded
aaronpim <44268127+aaronpim@users.noreply.github.com>
2023-02-17 11:41:54 +0000

    Add files via upload

--- Alt_Main.m
+++ Alt_Main.m
@@ -4,18 +4,16 @@
 % subcubes. We then iterate the foward Euler process n_iter times.
 
 N = 30;         %Spatial discretisation
-n_iter = 200;   %Number of iterations
+n_iter = 100;   %Number of iterations
 x = linspace(-1,1,N);           %1-D domain
                  %Spatial step
-
+Fokker_Plank =1;
 %% List of trajectory vectors.
 
 s_vec = combvec([-1,0,1],[-1,0,1],[-1,0,1])'; 
 s_vec = [s_vec(1:13,:);s_vec(15:end,:)]; 
 dx = (x(2)-x(1)).*sqrt(sum(s_vec.^2,2));
 s_vec = s_vec./repmat(sqrt(sum(s_vec.^2,2)),1,3);
-
-
 %% Array definition
 % The array f denotes the solution to the Boltzmann transport equation.
 f = zeros(N,N,N,length(s_vec));
@@ -27,7 +25,7 @@ for n = 1:n_iter
     for i = 1:length(s_vec)
         sctr = 0;        
         for j = 1:length(s_vec)
-            sctr = sctr + Sigma_s(x,s_vec(i,:),s_vec(j,:)).*f(:,:,:,j);
+            sctr = sctr + Sigma_s(x,s_vec(i,:),s_vec(j,:),Fokker_Plank).*f(:,:,:,j);
         end
         col = sctr - Sigma_t(x).*f(:,:,:,i); 
         % This step calculates the collision term.
@@ -49,13 +47,19 @@ u = sum(f,4);
 [X,Y,Z] = meshgrid(x,x,x);
 [Xmat,Ymat] = meshgrid(x,x);
 pcolor(Xmat-((x(2)-x(1))/2),Ymat-((x(2)-x(1))/2),u(:,:,round(end/2)));
-hold on
-plot(0.5*cos(linspace(0,2*pi,101)),0.5*sin(linspace(0,2*pi,101)),'r-')
-hold off
+if ~Fokker_Plank
+    hold on
+    plot(0.5*cos(linspace(0,2*pi,101)),0.5*sin(linspace(0,2*pi,101)),'r-')
+    hold off
+else
+    set(gca,'ColorScale','log')
+end
 axis equal
 figure;
-pcolor(Xmat-((x(2)-x(1))/2),Ymat-((x(2)-x(1))/2),reshape(u(round(end/2),:,:),length(Xmat),length(Xmat)));
-hold on
-plot(0.5*cos(linspace(0,2*pi,101)),0.5*sin(linspace(0,2*pi,101)),'r-')
-hold off
+pcolor(Ymat-((x(2)-x(1))/2),Xmat-((x(2)-x(1))/2),reshape(u(round(end/2),:,:),length(Xmat),length(Xmat)));
+if ~Fokker_Plank
+    hold on
+    plot(0.5*cos(linspace(0,2*pi,101)),0.5*sin(linspace(0,2*pi,101)),'r-')
+    hold off
+end
 axis equal
--- Sigma_s.m
+++ Sigma_s.m
@@ -1,14 +1,21 @@
-function [Sctr] = Sigma_s(x,s1,s2)
+function [Sctr] = Sigma_s(x,s1,s2,Fokker_Plank)
 [X,Y,Z] = meshgrid(x,x,x);
-R = sqrt(X.^2+Y.^2+Z.^2);
-
 dot_vec = [-1,-2/sqrt(6),-1/sqrt(2),-1/sqrt(3),-1/2,-1/3,0,1/3,1/2,1/sqrt(3),1/sqrt(2),2/sqrt(6),1];
-
-%prob_vec_inclusions = (1:length(dot_vec))./sum(1:length(dot_vec));
-prob_vec_inclusions = ones(length(dot_vec),1)/length(dot_vec);
-prob_vec_exclusions = [zeros(1,11),0.1,0.9];
-
-Sctr = (R>0.5).*dot(prob_vec_exclusions, (dot_vec==dot(s1,s2)) )...
+if Fokker_Plank
+    ds = norm([1,0,0]-[1,1,0]./sqrt(2));
+    if norm(s1-s2)==0
+        Sctr = -(4/ds.^2);
+    elseif norm(s1-s2)==ds
+        Sctr = (1/ds.^2);
+    else
+        Sctr = 0;
+    end
+else
+    R = sqrt(X.^2+Y.^2+Z.^2);
+    prob_vec_inclusions = ones(length(dot_vec),1)/length(dot_vec);
+    prob_vec_exclusions = [zeros(1,11),0.1,0.9];
+    Sctr = (R>0.5).*dot(prob_vec_exclusions, (dot_vec==dot(s1,s2)) )...
     +(R<=0.5).*dot(prob_vec_inclusions, (dot_vec==dot(s1,s2)) );
+end
 end
 
