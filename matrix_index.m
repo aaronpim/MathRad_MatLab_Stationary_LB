919b26581d524a32f8610df4fa565950ab38a935
aaronpim <44268127+aaronpim@users.noreply.github.com>
2023-03-02 13:52:44 +0000

    Update matrix_index.m

--- matrix_index.m
+++ matrix_index.m
@@ -1,4 +1,12 @@
 function [output] = matrix_index(value,N)
+%This function controls the indexing of the tensor for the Euler method
+% dot(s,grad(f)) ~= (f(x+dx*s)-f(x))/dx
+
+% f(x+dx*s) is represented by
+% f(matrix_index(s(1)),matrix_index(s(2)),matrix_index(s(3)))
+
+%Similiarly f(x) is reperesented by 
+% f(matrix_index(-s(1)),matrix_index(-s(2)),matrix_index(-s(3)))
 if sign(value)==0
     output = 1:N;
 elseif sign(value)==1
