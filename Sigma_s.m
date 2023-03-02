ec89de66801649bcf10d017ae502a0b153cceef5
aaronpim <44268127+aaronpim@users.noreply.github.com>
2023-03-02 22:55:10 +0000

    Update Sigma_s.m

--- Sigma_s.m
+++ Sigma_s.m
@@ -5,12 +5,11 @@ function [Sctr] = Sigma_s(x,s1,s2)
     s_dot_s = -5:5;
     N = length(s_dot_s);
     
-    prob_vec_inclusions = 1:N; 
+    prob_vec_inclusions = ones(N,1); 
     prob_vec_inclusions = prob_vec_inclusions./sum(prob_vec_inclusions);
-    prob_vec_exclusions = [1;zeros(10,1)]; 
+    prob_vec_exclusions = [1;zeros(N-1,1)]; 
     prob_vec_exclusions = prob_vec_exclusions./sum(prob_vec_exclusions);
     
     Sctr = (R>0.5).*dot(prob_vec_exclusions, s_dot_s==dot(s1,s2))...
          +(R<=0.5).*dot(prob_vec_inclusions, s_dot_s==dot(s1,s2));
 end
-
